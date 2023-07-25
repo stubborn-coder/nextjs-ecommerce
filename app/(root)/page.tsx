2c3e609a544ce90d15915bfa2ae65d87fad68e07
stubborn-coder <mbbilal0612@gmail.com>
2023-07-25 22:17:41 +0500

    added store-modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,14 +2,24 @@
 
 import { Button } from "@/components/ui/button";
 import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
 import { UserButton } from "@clerk/nextjs";
+import { useEffect } from "react";
 
 const SetupPage = () =>{
+    const onOpen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.isOpen);
+    // const storeModal = useStoreModal(); doesn't work fine inside of useEffect
+
+    useEffect(() => {
+      if(!isOpen){
+        onOpen();
+      }
+    }, [isOpen,onOpen])
+    
     return (
       <div className="p-4"> 
-        <Modal title="Test" description="Test desc" isOpen onClose={()=>{}}  >
-          Children
-        </Modal>
+        Root Pages
       </div>
         )
   }
